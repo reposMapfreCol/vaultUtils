03ea18948d27e79bad30e4e71735eb3fa8fd24c1
DCO002\PRLUISE <prluise@mapfre.com.co>
2025-10-23 17:18:44 -0500

    Commit base

---
+++ src/main/java/co/com/mapfre/vaultUtils/VaultUtilsApplication.java
@@ -0,0 +1,13 @@
+package co.com.mapfre.vaultUtils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultUtilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/mapfre/vaultUtils/VaultUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.mapfre.vaultUtils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
