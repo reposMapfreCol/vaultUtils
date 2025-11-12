6ae5270e19e9b0380b0bdd305771a80c961c6f7d
DCO002\PRLUISE <prluise@mapfre.com.co>
2025-11-12 09:13:34 -0500

    Creación de vault

--- src/main/java/co/com/mapfre/vaultUtils/utils/VaultUtil.java
+++ src/main/java/co/com/mapfre/vaultUtils/utils/VaultUtil.java
@@ -11,17 +11,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class VaultUtil {
 	
+	private String currentToken;
+	private long tokenExpiryTime; // En milisegundos
+
 	private final String vaultUrl;
 	private final String roleId;
 	private final String secretId;
 	private final String secretPath;
 	private final RestTemplate restTemplate;
 	private final ObjectMapper mapper;
-	
-	
-	
-	
-	 public VaultUtil(String vaultUrl, String roleId, String secretId, String secretPath, RestTemplate restTemplate, ObjectMapper mapper) {
+
+	public VaultUtil(String vaultUrl, String roleId, String secretId, String secretPath, RestTemplate restTemplate,
+			ObjectMapper mapper) {
 		this.vaultUrl = vaultUrl;
 		this.roleId = roleId;
 		this.secretId = secretId;
@@ -30,38 +31,46 @@ public class VaultUtil {
 		this.mapper = mapper;
 	}
 
-	 private String getVaultToken() {
-	        try {
-	            String url = vaultUrl + "/v1/auth/approle/login";
-	            String payload = String.format("{\"role_id\": \"%s\", \"secret_id\": \"%s\"}", roleId, secretId);
-	            HttpHeaders headers = new HttpHeaders();
-	            headers.setContentType(MediaType.APPLICATION_JSON);
-	            HttpEntity<String> request = new HttpEntity<>(payload, headers);
+	private void authenticate() {
+		try {
+			String url = vaultUrl + "/v1/auth/approle/login";
+			String payload = String.format("{\"role_id\": \"%s\", \"secret_id\": \"%s\"}", roleId, secretId);
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			HttpEntity<String> request = new HttpEntity<>(payload, headers);
+			
+			ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
+			JsonNode json = mapper.readTree(response.getBody());
+			currentToken = json.get("auth").get("client_token").asText();
+			long ttl = json.get("auth").get("lease_duration").asLong(); // segundos
+			tokenExpiryTime = System.currentTimeMillis() + (ttl * 1000);
+			
+		} catch (Exception e) {
+			throw new RuntimeException("Error autenticando con Vault: " + e.getMessage(), e);
+		}
+	}
 
-	            ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-	            JsonNode json = mapper.readTree(response.getBody());
-	            return json.get("auth").get("client_token").asText();
-	        } catch (Exception e) {
-	            throw new RuntimeException("Error autenticando con Vault: " + e.getMessage(), e);
-	        }
+	public String getSecretValue(String key) {
+		try {
+			renewTokenIfNeeded();
+		    String url = vaultUrl + "/v1/" + secretPath;
+		    HttpHeaders headers = new HttpHeaders();
+		    headers.setBearerAuth(currentToken);
+		    HttpEntity<Void> request = new HttpEntity<>(headers);
+
+		    ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, request, String.class);
+		    JsonNode json = mapper.readTree(response.getBody());
+		    
+		    return json.at("/data/data/" + key).asText();
+		    
+		} catch (Exception e) {
+			throw new RuntimeException("Error leyendo secreto desde Vault: " + e.getMessage(), e);
+		}
+	}
+	
+	private void renewTokenIfNeeded() {
+	    if (System.currentTimeMillis() > tokenExpiryTime - 60000) { // 1 min antes
+	        authenticate();
 	    }
-
-	    public String getSecretValue(String key) {
-	        try {
-	            String token = getVaultToken();
-	            String url = vaultUrl + "/v1/" + secretPath;
-
-	            HttpHeaders headers = new HttpHeaders();
-	            headers.setBearerAuth(token);
-	            HttpEntity<Void> request = new HttpEntity<>(headers);
-
-	            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, request, String.class);
-	            JsonNode json = mapper.readTree(response.getBody());
-
-	            return json.at("/data/data/" + key).asText();
-	        } catch (Exception e) {
-	            throw new RuntimeException("Error leyendo secreto desde Vault: " + e.getMessage(), e);
-	        }
-	    }
-
+	}
 }
